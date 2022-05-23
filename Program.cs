5f89e6a07da632ae8b8f5b8d49be004adbe392c9
nurfadhlurrahman@students.amikom.ac.id <nurfadhlurrahman@students.amikom.ac.id>
2022-05-23 10:37:12 +0700

    Nomor 4

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nomor4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dosen Dosen1 = new dosen();
+            Dosen1.attDosen(1, "Fadhlur", 21114349);
+            Dosen1.addCourse("Pemrograman");
+            Dosen1.Tampilan();
+            Console.WriteLine();
+
+            dosen Dosen2 = new dosen();
+            Dosen2.Tampilan();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dosen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nomor4
+{
+    class dosen
+    {
+        public int id { get; set; }
+        public string nama { get; set; }
+        public int Nik { get; set; }
+        public string gender { get; set; }
+        public string course { get; set; }
+
+        public dosen()
+        {
+            this.id = 2;
+            this.nama = "Kamarudin, M.kom";
+            this.Nik = 0525038203;
+            this.gender = "Laki-laki";
+            this.course = "Pemrograman";
+        }
+        public void attDosen(int id, string name, int nik)
+        {
+            this.id = id;
+            this.nama = name;
+            this.Nik = nik;
+        }
+        public void addCourse(string course)
+        {
+            this.course = course;
+        }
+        public void Tampilan()
+        {
+            Console.WriteLine("Id     = {0}", id);
+            Console.WriteLine("Nama   = {0}", nama);
+            Console.WriteLine("NIK    = {0}", Nik);
+            Console.WriteLine("Gender = {0}", gender);
+            Console.WriteLine("Course = {0}", course);
+
+        }
+    }
+}
